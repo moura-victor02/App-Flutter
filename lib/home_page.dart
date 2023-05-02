aacb1370396dc4bb1357d5dd007f05fd35dd3d3a
Enzo Victor <enzomoru3@gmail.com>
2023-05-02 12:35:13 -0300

    att

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,41 +9,83 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
-        appBar: AppBar(
-          title: Text('BarCode Scanner'),
+      appBar: AppBar(
+        title: Text('Leitor de Código de Barras'),
+        backgroundColor: theme.primaryColor,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Card(
+          elevation: 4.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16.0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Valor do código de barras:',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: theme.primaryColor,
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                GetBuilder<HomePageController>(
+                  builder: (controller) {
+                    return Text(
+                      controller.valorCodigoBarras,
+                      style: theme.textTheme.bodyText1,
+                    );
+                  },
+                ),
+                SizedBox(height: 24.0),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: theme.primaryColor,
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
+                        child: InkWell(
+                          onTap: () {
+                            Get.find<HomePageController>()
+                                .escanearCodigodeBarras();
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 16.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.qr_code_scanner,
+                                  color: theme.colorScheme.onPrimary,
+                                ),
+                                SizedBox(width: 8.0),
+                                Text(
+                                  'Ler código de barras',
+                                  style: TextStyle(
+                                    color: theme.colorScheme.onPrimary,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ),
-        body: SizedBox.expand(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Text('Valor código de barras:',
-                style: TextStyle(
-                    fontWeight: FontWeight.normal,
-                    color: Colors.red,
-                    fontSize: 25)),
-            GetBuilder<HomePageController>(
-              builder: (controller) {
-                return Text(controller.valorCodigoBarras,
-                    style:
-                        TextStyle(fontWeight: FontWeight.normal, fontSize: 15));
-              },
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextButton.icon(
-              icon: Image.asset(
-                'assets/icon.png',
-                width: 50,
-              ),
-              label: Text(
-                'Ler código de barras',
-                style: TextStyle(fontWeight: FontWeight.normal, fontSize: 15),
-              ),
-              onPressed: () {
-                Get.find<HomePageController>().escanearCodigodeBarras();
-              },
-            )
-          ]),
-        ));
+      ),
+    );
   }
 }
