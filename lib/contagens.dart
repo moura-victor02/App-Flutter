5028bab6a15341268d16b439e38f5c625a111c0c
Enzo Victor <enzomoru3@gmail.com>
2023-05-06 20:26:22 -0300

    att

---
+++ lib/contagens.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:app01/page_scanner.dart';
+import 'api_service.dart';
+
+class Contagem extends StatefulWidget {
+  @override
+  _ContagemState createState() => _ContagemState();
+}
+
+class _ContagemState extends State<Contagem> {
+  final _formKey = GlobalKey<FormState>();
+  final _apiService = ApiService();
+  final List<Map<String, dynamic>> buttons = [
+    {
+      'text': 'Contagem 1',
+      'icon': Icons.inventory,
+      'color': Colors.blue,
+      'textColor': Colors.white,
+    },
+    {
+      'text': 'Contagem 2',
+      'icon': Icons.inventory,
+      'color': Colors.green,
+      'textColor': Colors.white,
+    },
+    {
+      'text': 'Contagem 3',
+      'icon': Icons.inventory,
+      'color': Colors.purple,
+      'textColor': Colors.white,
+    },
+  ];
+  Future<void> sendDataToProtheus(String barcodeData) async {
+    await _apiService.sendBarcodeData(barcodeData);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contagem'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: FractionallySizedBox(
+          widthFactor: 0.8,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                alignment: Alignment.center,
+                child: Text(
+                  'Escolha a contagem desejada:',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 17,
+                    fontFamily: 'Montserrat',
+                    color: Colors.black87,
+                    height: 1.3,
+                  ),
+                ),
+              ),
+              ListView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: buttons.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => BarcodeScannerPage(
+                            apiService: _apiService,
+                          ),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      height: 80,
+                      padding: EdgeInsets.symmetric(horizontal: 16),
+                      margin: EdgeInsets.only(bottom: 16),
+                      decoration: BoxDecoration(
+                        color: buttons[index]['color'],
+                        borderRadius: BorderRadius.circular(8),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.3),
+                            blurRadius: 8,
+                            offset: Offset(0, 2),
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            buttons[index]['icon'],
+                            color: Colors.white,
+                            size: 40,
+                          ),
+                          SizedBox(width: 16),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                buttons[index]['text'],
+                                style: TextStyle(
+                                  color: buttons[index]['textColor'],
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 24,
+                                  fontFamily: 'Montserrat',
+                                  height: 1.2,
+                                ),
+                              ),
+                              SizedBox(height: 4),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/page_scanner.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
+import 'api_service.dart';
+
+class BarcodeScannerPage extends StatefulWidget {
+  final ApiService apiService;
+  final String cancelButtonText = 'Cancelar';
+  BarcodeScannerPage({required this.apiService});
+
+  @override
+  _BarcodeScannerPageState createState() => _BarcodeScannerPageState();
+}
+
+class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
+  final _barcodeController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  Future<String> scanBarcode() async => await FlutterBarcodeScanner.scanBarcode(
+        '#ff6666',
+        'Cancelar',
+        true,
+        ScanMode.BARCODE,
+      );
+
+  Future<void> sendDataToProtheus(String barcodeData) async {
+    await widget.apiService.sendBarcodeData(barcodeData);
+  }
+
+  @override
+  void dispose() {
+    _barcodeController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Leitor de código de barras'),
+        centerTitle: true,
+        leading: Icon(Icons.bar_chart),
+      ),
+      body: Center(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextFormField(
+                  controller: _barcodeController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Digite o código ou utilize o botão de leitura',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Por favor, digite o código de barras';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              SizedBox(height: 24),
+              ElevatedButton(
+                onPressed: () async {
+                  if (_formKey.currentState!.validate()) {
+                    await sendDataToProtheus(_barcodeController.text);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content:
+                            Text('Dados enviados para o Protheus com sucesso!'),
+                      ),
+                    );
+                    _barcodeController.clear();
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  elevation: 3,
+                  padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.save,
+                      size: 36,
+                      color: Colors.white,
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      'Salvar',
+                      style: TextStyle(fontSize: 17),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 40),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.7,
+                height: 60,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(24.0),
+                  gradient: LinearGradient(
+                    colors: [Colors.blue, Colors.lightBlue],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 7,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: ElevatedButton.icon(
+                  onPressed: () async {
+                    String barcodeData = await scanBarcode();
+                    if (barcodeData != '-1') {
+                      setState(() {
+                        _barcodeController.text = barcodeData;
+                      });
+                    } else {
+                      setState(() {
+                        _barcodeController.clear();
+                      });
+                    }
+                  },
+                  icon: Icon(
+                    Icons.qr_code_scanner,
+                    color: Colors.white,
+                  ),
+                  label: Text(
+                    'Ler código de barras',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.transparent,
+                    onPrimary: Colors.transparent,
+                    elevation: 0,
+                    padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24.0),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
