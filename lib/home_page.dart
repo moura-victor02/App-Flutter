01139b72afd6efc6a069a29d922868576951be0d
Enzo Victor <enzomoru3@gmail.com>
2023-05-14 11:45:46 -0300

    att

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,12 +1,89 @@
 import 'package:flutter/material.dart';
-import 'Inventario/contagens.dart';
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
+//Classe que define dados do botão, ou seja, strings referentes a construção//
+class ButtonData {
+  final String text;
+  final IconData icon;
+  final Color color;
+  final Color textColor;
+  final String route;
+
+  ButtonData({
+    required this.text,
+    required this.icon,
+    required this.color,
+    required this.textColor,
+    required this.route,
+  });
+}
+
+//Lista de strings referente as paginas que estão definas as rotas dentro do MaterialApp//
+final List<String> routes = [
+  '/contagem',
+  '/exemplo1',
+  '/exemplo2',
+  '/exemplo3',
+  '/exemplo4',
+  '/exemplo5',
+];
+
+//Cada botão é um mapa com várias propriedades, como texto, ícone, cor de fundo, cor do texto e rota a ser navegada ao clicar no botão//
 class _HomePageState extends State<HomePage> {
+  List<Map<String, dynamic>> buttons = [
+    {
+      'text': 'Contabilizador de Inventário',
+      'icon': Icons.inventory,
+      'color': Color.fromARGB(255, 63, 70, 73),
+      'textColor': Colors.white,
+      'route': routes[0],
+    },
+    {
+      'text': '...',
+      'icon': Icons.help_outline,
+      'color': Color.fromARGB(255, 63, 70, 73),
+      'textColor': Colors.white,
+      //'route': routes[1],
+    },
+    {
+      'text': '...',
+      'icon': Icons.help_outline,
+      'color': Color.fromARGB(255, 63, 70, 73),
+      'textColor': Colors.white,
+      //'route': routes[2],
+    },
+    {
+      'text': '...',
+      'icon': Icons.help_outline,
+      'color': Color.fromARGB(255, 63, 70, 73),
+      'textColor': Colors.white,
+      //'route': routes[3],
+    },
+    {
+      'text': '...',
+      'icon': Icons.help_outline,
+      'color': Color.fromARGB(255, 63, 70, 73),
+      'textColor': Colors.white,
+      //'route': routes[4],
+    },
+    {
+      'text': '...',
+      'icon': Icons.help_outline,
+      'color': Color.fromARGB(255, 63, 70, 73),
+      'textColor': Colors.white,
+      //'route': routes[5],
+    },
+  ];
+
+//responsável por navegar para a rota especificada no parâmetro route//
+  void _navigateToRoute(String route) {
+    Navigator.pushNamed(context, route);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,86 +92,37 @@ class _HomePageState extends State<HomePage> {
         title: Text('Gerenciamento'),
         centerTitle: true,
       ),
-      body: Row(
-        children: [
-          Expanded(
-            child: Align(
-              alignment: Alignment.topLeft,
-              child: FractionallySizedBox(
-                widthFactor: 0.97,
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => Contagem(),
-                      ),
-                    );
-                  },
-                  child: Container(
-                    height: 135,
-                    decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 63, 70, 73),
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        Icon(
-                          Icons.inventory,
-                          color: Colors.red,
-                          size: 53,
-                        ),
-                        Positioned(
-                          bottom: 0,
-                          child: Text(
-                            'Contabilizador de Inventário',
-                            style: TextStyle(fontSize: 14, color: Colors.white),
-                          ),
-                        ),
-                      ],
-                    ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        padding: EdgeInsets.all(10),
+        children: buttons.map((button) {
+          return GestureDetector(
+            onTap: () => {_navigateToRoute(button['route'])},
+            child: Container(
+              decoration: BoxDecoration(
+                color: button['color'],
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    button['icon'],
+                    color: Colors.red,
+                    size: 55,
                   ),
-                ),
+                  SizedBox(height: 33),
+                  Text(
+                    button['text'],
+                    style: TextStyle(fontSize: 11, color: button['textColor']),
+                  ),
+                ],
               ),
             ),
-          ),
-          Expanded(
-            child: Align(
-              alignment: Alignment.topRight,
-              child: FractionallySizedBox(
-                widthFactor: 0.97,
-                child: GestureDetector(
-                  onTap: () {},
-                  child: Container(
-                    height: 135,
-                    decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 63, 70, 73),
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        Icon(
-                          Icons.help_outline,
-                          color: Colors.red,
-                          size: 53,
-                        ),
-                        Positioned(
-                          bottom: 0,
-                          child: Text(
-                            'Lista de Produtos',
-                            style: TextStyle(fontSize: 14, color: Colors.white),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
+          );
+        }).toList(),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:app01/home_page.dart';
 import 'package:flutter/material.dart';
+import 'Inventario/contagens.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,11 +10,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Leitor de código de barras',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(),
+      title: 'Minha aplicação',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+        '/contagem': (context) => Contagem(),
+        /*'/exemplo1': (context) => Exemplo1(),
+        '/exemplo2': (context) => Exemplo2(),
+        '/exemplo3': (context) => Exemplo3(),
+        '/exemplo4': (context) => Exemplo4(),
+        '/exemplo5': (context) => Exemplo5(),*/
+      },
     );
   }
 }
